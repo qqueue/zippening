2a73f86ca047c83be5673767bc1005436e419c91
queue <queue@hakase.org>
2016-02-14 13:24:57 -0700

    Easier scripting, error output

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
 import qualified Data.ByteString.Lazy as B
+import System.Environment (getArgs)
 import Data.Binary.Get
 import Data.Aeson
 
 main :: IO ()
-main = B.interact $
-  encode . (map entryMetadata) . rarEntries . (runGet getRarArchive)
+main = do
+  (filePath:_) <- getArgs
+  input <- B.readFile filePath
+  let rar = runGet (label ("reading: " ++ filePath) getRarArchive) input
+  let meta = (map entryMetadata) $ rarEntries rar
+  B.putStr $ encode $ object 
+    [ "entries" .= meta
+    , "file" .= filePath
+    ]
   
